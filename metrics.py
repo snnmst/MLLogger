5e464d29951daceda52deeda5b3561dcbf50b725
= <=>
2023-10-01 09:06:14 +0000

    updated with regression and classification metrics

--- metrics.py
+++ metrics.py
@@ -1,4 +1,5 @@
 from sklearn.metrics import accuracy_score, f1_score, roc_auc_score, confusion_matrix
+from sklearn.metrics import mean_absolute_error, mean_squared_error
 
 class Metrics:
     def __init__(self, y_true, y_pred, y_pred_prob=None):
@@ -17,3 +18,12 @@ class Metrics:
 
     def evaluate_confusion_matrix(y_true, y_pred, y_pred_prob=None):
         return confusion_matrix(y_true, y_pred)
+
+    def evaluate_mean_absolute_error(y_true, y_pred, y_pred_prob=None):
+        return mean_absolute_error(y_true, y_pred)
+
+    def evaluate_mean_squared_error(y_true, y_pred, y_pred_prob=None):
+        return mean_squared_error(y_true, y_pred)
+
+    def evaluate_root_mean_squared_error(y_true, y_pred, y_pred_prob=None):
+        return mean_squared_error(y_true, y_pred, squared = False)
